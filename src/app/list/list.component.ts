e83e0ae95fd6de4cf471151ea2c202a588305c8f
Flávio Vinícius Martins dos Santos <vinicius0809@gmail.com>
2023-05-31 18:28:20 -0300

    Changes based on database

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import {
   Firestore,
-  docData,
-  doc
+  collectionData
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { collection } from 'firebase/firestore';
+import { Observable, map } from 'rxjs';
 
 interface ListElement {
   minute: number;
@@ -35,10 +35,11 @@ interface RootList {
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
-  tableData2!: RootList;
+  // tableData2!: RootList;
   selectedHour: number;
-  public readonly testDocValue$: Observable<any>;
-  tableData: List[] = [];
+  // public readonly testDocValue$: Observable<any>;
+  // tableData: List[] = [];
+  lists$: Observable<List[]>;
 
   @Input()
   get hour(){
@@ -47,33 +48,40 @@ export class ListComponent {
 
   constructor(private firestore: Firestore) {
     this.selectedHour = new Date().getHours();
-    console.log("lists/" + this.getDateStringFormated(new Date()));
-    const ref = doc(firestore, "lists/" + this.getDateStringFormated(new Date()));
-    this.testDocValue$ = docData(ref);
-    this.testDocValue$.subscribe(x => {
-      console.log(x);
-      this.tableData2 = x;
-      this.updateTableData();
-    });
+    const insideListsCollection = collection(this.firestore, `lists/${this.getDateStringFormated(new Date())}_${this.selectedHour}/inside-lists`)
+    this.lists$ = collectionData(insideListsCollection) as Observable<List[]>;
+    this.lists$ = this.lists$.pipe(
+      map(r => {
+        return r.sort((a,b) => b.listBalance - a.listBalance);
+      })
+    );
+
+    // const ref = doc(this.firestore, "lists/" + this.getDateStringFormated(new Date()));
+    // this.testDocValue$ = docData(ref);
+    // this.testDocValue$.subscribe(x => {
+    //   console.log(x);
+    //   this.tableData2 = x;
+    //   this.updateTableData();
+    // });
   }
 
-  updateTableData() {
-    console.log("Entrou " + this.selectedHour);
-    for (let i = 0; i < 24; i++) {
-      if (this.tableData2[i] != undefined && i == this.selectedHour) {        
-        this.tableData2[i].sort((a,b) => b.listBalance - a.listBalance);
-        this.tableData2[i].forEach(listObj => {
-          const index = this.tableData.findIndex(x => x.listHour == listObj.listHour && x.listSize == listObj.listSize && x.inverse == listObj.inverse);
-          if (index == -1) {
-            this.tableData.push(listObj);
-          }
-          else {
-            this.tableData[index] = listObj;
-          }
-        })
-      }
-    }
-  }
+  // updateTableData() {
+  //   console.log("Entrou " + this.selectedHour);
+  //   for (let i = 0; i < 24; i++) {
+  //     if (this.tableData2[i] != undefined && i == this.selectedHour) {        
+  //       this.tableData2[i].sort((a,b) => b.listBalance - a.listBalance);
+  //       this.tableData2[i].forEach(listObj => {
+  //         const index = this.tableData.findIndex(x => x.listHour == listObj.listHour && x.listSize == listObj.listSize && x.inverse == listObj.inverse);
+  //         if (index == -1) {
+  //           this.tableData.push(listObj);
+  //         }
+  //         else {
+  //           this.tableData[index] = listObj;
+  //         }
+  //       })
+  //     }
+  //   }
+  // }
 
   getDateStringFormated(date: Date) {
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
