102824156446d7442f95a357bef6ae92968fbe96
Flávio Vinícius Martins dos Santos <vinicius0809@gmail.com>
2023-06-22 09:43:02 -0300

    Change list considering algorithm

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -34,7 +34,8 @@ interface List {
 })
 export class ListComponent {
   selectedHour: number;
-  totalWinsG7: number = 0; 
+  totalWinsG7: number = 0;
+  totalLoss: number = 0;
   lists$: Observable<List[]> = new Observable<List[]>();
 
   @Input()
@@ -65,15 +66,18 @@ export class ListComponent {
       map((r) => {
         if (r.length > 1) {
           this.totalWinsG7 = 0;
+          this.totalLoss = 0;
           let consideredList = -1;
           for (let i = 0; i < r[0].listElements.length; i++) {
-            const element1 = r[0].listElements[i];
-            const element2 = r[1].listElements[i];
+            const elements1 = [r[0].listElements[i], r[0].listElements[i - 1], r[0].listElements[i - 2]];
+            const elements2 = [r[1].listElements[i], r[1].listElements[i - 1], r[1].listElements[i - 2]];
+            const element1 = elements1[0];
+            const element2 = elements2[0];
             element1.consideredWin = "";
             element2.consideredWin = "";
 
             if (consideredList > -1) {
-              if(r[consideredList].listElements[i].result == ""){
+              if (r[consideredList].listElements[i].result == "") {
                 r[consideredList].listElements[i].consideredWin = "🕖"
               }
               else if (r[consideredList].listElements[i].result != "❌") {
@@ -81,30 +85,49 @@ export class ListComponent {
                 this.totalWinsG7++;
               }
               else if (r[consideredList].listElements[i].result == "❌") {
-                r[consideredList].listElements[i].consideredWin = "🔺"
+                r[consideredList].listElements[i].consideredWin = "🔺";
+                this.totalLoss++;
               }
             }
 
             element1.actualGradientText = getActualGradientText(element1.actualGradient);
             element2.actualGradientText = getActualGradientText(element2.actualGradient);
+            element1.considered = ""
+            element2.considered = ""
+            consideredList = -1;
 
-            if (element1.result != "" && element1.actualGradient - element2.actualGradient > gradientLimiar) {
-              element1.considered = "🟦";
-              consideredList = 0;
+            if (i < 2) {
+              if (element1.result != "" && element1.actualGradient - element2.actualGradient > gradientLimiar) {
+                element1.considered = "🟦";
+                consideredList = 0;
+              }
+              else if (element2.result != "" && element2.actualGradient - element1.actualGradient > gradientLimiar) {
+                element2.considered = "🟦";
+                consideredList = 1;
+              }
             }
-            else if (element2.result != "" && element2.actualGradient - element1.actualGradient > gradientLimiar) {
-              element2.considered = "🟦";
-              consideredList = 1;
-            } else {
-              element1.considered = ""
-              element2.considered = ""
-              consideredList = -1;
+            else {
+              const twoConsecutiveLosses1 = this.returnConsecutivePattern(elements1);
+              const twoConsecutiveLosses2 = this.returnConsecutivePattern(elements2);
+              if (element1.result != "" && (elements1[1].considered == "🟦" && !twoConsecutiveLosses1 || twoConsecutiveLosses2)) {
+                element1.considered = "🟦";
+                consideredList = 0;
+              }
+              else if (element2.result != "" && (elements2[1].considered == "🟦" && !twoConsecutiveLosses2 || twoConsecutiveLosses1)) {
+                element2.considered = "🟦";
+                consideredList = 1;
+              }
             }
           }
         }
         return r.sort((a, b) => b.listGradient - a.listGradient);
       })
     );
+  }
+
+  private returnConsecutivePattern(elements: ListElement[]) {
+    return (["❌", "✅", "✅(⚪️)"].includes(elements[0].result) && ["❌", "✅", "✅(⚪️)"].includes(elements[1].result) && ["❌"].includes(elements[2].result))
+      || (["❌", "✅", "✅(⚪️)"].includes(elements[0].result) && ["❌"].includes(elements[1].result) && ["❌", "✅", "✅(⚪️)"].includes(elements[2].result));
   }
 
   getDateStringFormatted(date: Date) {
